819c4ddcbd51f2929b7ff387d7ca48df43288338
Nathaniel Kelso <nvkelso@gmail.com>
2016-01-07 17:11:39 -0800

    track external editors via GA events

--- map/main.js
+++ map/main.js
@@ -170,8 +170,8 @@ map = (function () {
 	                popup.style.visibility = 'visible';
 	            }
                 popup.innerHTML = '<span class="labelInner">' + 'You found an airport that needs help!' + '</span><br>';
-                popup.innerHTML += '<span class="labelInner">' + '<a target="_blank" href="' + url + '">Edit with iD ➹</a>' + '</span><br>';
-                popup.innerHTML += '<span class="labelInner">' + '<a target="_blank" href="' + josmUrl + '">Edit with JOSM ➹</a>' + '</span><br>';
+                popup.innerHTML += '<span class="labelInner">' + '<a target="_blank" href="' + url + '" onclick="trackOutboundLink("' + url + '", residential_buildings, iD); return false;">Edit with iD ➹</a>' + '</span><br>';
+                popup.innerHTML += '<span class="labelInner">' + '<a target="_blank" href="' + josmUrl + '" onclick="trackOutboundLink("' + josmUrl + '", residential_buildings, JOSM); return false;">Edit with JOSM ➹</a>' + '</span><br>';
             });
         });
 
